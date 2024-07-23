21c0460e10b2af703e8db17cf3caa123b0792a8b
Klahadore <stringycheese@protonmail.com>
2024-07-23 16:14:05 -0400

    to device

--- 3dGroupUnet.py
+++ 3dGroupUnet.py
@@ -136,12 +136,14 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
+    model.to(device)
     for epoch in range(num_epochs):
         model.train()
 
         for images, masks, in train_loader:
 
+            images.to(device)
+            masks.to(device)
 
             outputs = model(images)
             loss = criterion(outputs, masks)
